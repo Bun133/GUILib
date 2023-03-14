3145e92c35b206b79f74235ae08e1261fe30c16a
bun133 <bun133.work@gmail.com>
2023-03-14 20:37:30 +0900

    Init

--- guilib/build.gradle.kts
+++ guilib/build.gradle.kts
@@ -10,12 +10,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation(kotlin("stdlib-jdk8"))
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
 }
 
 tasks.getByName<Test>("test") {
