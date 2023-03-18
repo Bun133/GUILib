76d5b71e46faf9020e67e72e8a907c6698f96fdb
bun133 <bun133.work@gmail.com>
2023-03-18 10:14:42 +0900

    最悪なフォルダ構造を直しました

---
+++ sample/src/main/java/com/github/bun133/guilib/sample/Sample.java
@@ -0,0 +1,17 @@
+package com.github.bun133.guilib.sample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sample extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
