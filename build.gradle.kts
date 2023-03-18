6d912a1c14714cb5b770eb666503b34b88004bde
bun133 <bun133.work@gmail.com>
2023-03-18 10:27:28 +0900

    頼むJitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,9 @@ tasks.getByName<Test>("test") {
 }
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
 }
